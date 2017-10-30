ee2562f76491924b1499dc957ed2c5748b3f7f77
dhtang <dhtang@localhost>
2017-10-30 02:07:06 -0500

    finished

--- src/main/java/edu/university/dhtang/Homework3/MyList.java
+++ src/main/java/edu/university/dhtang/Homework3/MyList.java
@@ -1,6 +1,15 @@
 package edu.university.dhtang.Homework3;
 import java.util.Scanner;
 
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 class Element implements Comparable<Element> {
 	Integer num;
 	String str;
@@ -44,22 +53,42 @@ public class MyList{
 		return false;	
 	}
 	
-    public static void main(String[] args){
+    public static void main(String[] args) throws ParseException{
+        Options options = new Options();
+        CommandLineParser parser =  new DefaultParser();
+        
+        Option type = new Option("t", "type", true, "type of entry");
+        Option key = new Option("k", "key", true, "key for list");
+        Option list = new Option("l", "list", true, "list of entries");
+        options.addOption(type);
+        options.addOption(key);
+        list.setArgs(Option.UNLIMITED_VALUES);
+        options.addOption(list);
 
-    	Element[] elementArray = new Element[4];
-    	
-    	Element a = new Element(3);
-    	Element b = new Element(4);
-    	Element c = new Element(5);
-    	Element d = new Element(6);
-    	
-    	elementArray[0] = a;
-    	elementArray[1] = b;
-    	elementArray[2] = c;
-    	elementArray[3] = d;
-    	
-    	Element key = new Element(6);
-        boolean result = binSearch(elementArray, key);
+        CommandLine cmd = parser.parse(options, args);
+        String listType = cmd.getOptionValue("t");
+        String[] newList = cmd.getOptionValues("l");
+        Element[] elementArray = new Element[newList.length];
+        String inputKey = cmd.getOptionValue("k");
+        boolean result = false;
+        
+        if (listType == "i") {
+        	for(int i = 0; i < newList.length; i++) {
+        		Element input = new Element(Integer.parseInt(newList[i]));
+        		elementArray[i] = input;
+        	}
+        	Element listKey = new Element(Integer.parseInt(inputKey));
+            result = binSearch(elementArray, listKey);
+        }
+        else {
+        	for(int i = 0; i < newList.length; i++) {
+        		Element input = new Element(newList[i]);
+        		elementArray[i] = input;
+        	}
+        	Element listKey = new Element(inputKey);
+        	result = binSearch(elementArray, listKey);
+        }
+
 
         if(result == true) {
         	System.out.println(1);
